365186ca5542027eb6d30d5624750aa8a377cfd7
ronak bendwal <ronakbendwal7880@gmail.com>
2025-08-23 23:44:50 +0530

    here subscriber model created

---
+++ src/Models/Subscription.Model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import express,{ Schema,model } from "mongoose";
+
+const SubscriptionSchema=new Schema({
+  subscriber:{
+    type:mongoose.Types.ObjectId,//one who is subscribing is here 
+    ref:"UserModel",
+  },
+  channel:{
+    type:mongoose.Types.ObjectId,//one to whome subscriber is subscribing
+    ref:"UserModel"
+  }
+},{timestamps:true})
+
+export const SubscriptionModel=model("SubscriptionModel",SubscriptionSchema)
